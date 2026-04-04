2238a0d996c03b68ebeb6148025ffb1d597c7997
ThanhTan <thanhtan27032001@gmail.com>
2026-04-04 23:24:55 +0700

    Feat add Shopping Item

--- app/src/main/java/com/gaden/shoppinglist/MainActivity.kt
+++ app/src/main/java/com/gaden/shoppinglist/MainActivity.kt
@@ -4,14 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.gaden.shoppinglist.ui.theme.ShoppingListTheme
+import kotlin.collections.listOf
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +35,21 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShoppingListTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+                    modifier = Modifier.fillMaxSize().padding(top = 32.dp),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ShoppingListApp()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppingListTheme {
-        Greeting("Android")
+
     }
 }
---
+++ app/src/main/java/com/gaden/shoppinglist/ShoppingList.kt
@@ -0,0 +1,160 @@
+package com.gaden.shoppinglist
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+
+data class ShoppingItem(
+    val id: Int,
+    var name: String,
+    var quality: Int,
+    var isEditing: Boolean = false
+)
+
+class ShoppingList {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ShoppingListApp() {
+    var shoppingItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
+    var isShowDialog by remember { mutableStateOf(false) }
+    var itemName by remember { mutableStateOf("") }
+    var itemQuality by remember { mutableStateOf("0") }
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(
+            onClick = { isShowDialog = true },
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        ) {
+            Text("Add item")
+        }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            items(shoppingItems) {
+                ShoppingListItem(
+                    item = it,
+                    onEditClicked = {},
+                    onDeleteClicked = {}
+                )
+            }
+        }
+    }
+
+    if (isShowDialog) {
+        AlertDialog(
+            onDismissRequest = { isShowDialog = false },
+            confirmButton = {
+                Button(onClick = {
+                    if (itemName.isNotBlank()) {
+                        val newItem = ShoppingItem(
+                            id = shoppingItems.size + 1,
+                            name = itemName,
+                            quality = itemQuality.toIntOrNull() ?: 0
+                        )
+                        shoppingItems = shoppingItems + newItem
+                        isShowDialog = false
+                        itemName = ""
+                        itemQuality = "0"
+                    }
+                }) {
+                    Text("Add")
+                }
+            },
+            dismissButton = {
+                Button(onClick = {}) {
+                    Text("Cancel")
+                }
+            },
+            title = {
+                Text("Add Shopping item")
+            },
+            text = {
+                Column {
+                    OutlinedTextField(
+                        value = itemName,
+                        placeholder = { Text("Item name") },
+                        onValueChange = {
+                            itemName = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(Modifier.height(8.dp))
+                    OutlinedTextField(
+                        value = itemQuality,
+                        placeholder = { Text("Item quantity") },
+                        onValueChange = {
+                            itemQuality = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth(),
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number,
+                        )
+                    )
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun ShoppingListItem(
+    item: ShoppingItem,
+    onEditClicked: () -> Unit,
+    onDeleteClicked: () -> Unit
+) {
+    Box {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .border(
+                    border = BorderStroke(
+                        1.dp,
+                        color = MaterialTheme.colorScheme.primary
+                    ),
+                    shape = RoundedCornerShape(8.dp)
+                )
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(item.name)
+            Text(item.quality.toString())
+        }
+    }
+}
